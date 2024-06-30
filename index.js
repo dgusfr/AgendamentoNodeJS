00155dceda110f1db51aaf07508d42d7e6d98478
dgusfr <diegogustavo.fr@gmail.com>
2024-06-30 09:26:45 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+// Configuração
+app.use(express.static("public"));
+
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// EJS
+app.set("view engine", "ejs");
+
+// Conexão com o banco de dados
+mongoose.connect("mongodb://localhost:27017/agendamento", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Model - Usuários
+app.get("/", (req, res) => {
+  res.send("Olá, Mundo!");
+});
+
+// Servidor
+app.listen(8080, () => {});
