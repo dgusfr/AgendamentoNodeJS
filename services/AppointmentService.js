f3f007d62a2060dd54ca49f27d5d172725d2f722
dgusfr <diegogustavo.fr@gmail.com>
2024-07-01 09:26:42 -0300

    add mongoose

--- services/AppointmentService.js
+++ services/AppointmentService.js
@@ -1,3 +1,8 @@
-class AppointmentService {}
+var appointment = require("../models/Appointment");
+var mongoose = require("mongoose");
+
+class AppointmentService {
+  async(name, email, description, cpf, date, time) {}
+}
 
 module.exports = new AppointmentService();
