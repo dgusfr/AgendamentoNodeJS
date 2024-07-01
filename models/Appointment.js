7fe1a8d21111052be59ef06656d8d133f6fed191
dgusfr <diegogustavo.fr@gmail.com>
2024-06-30 21:50:08 -0300

    add appointment

---
+++ models/Appointment.js
@@ -0,0 +1,13 @@
+const { name } = require("ejs");
+const mongoose = require("mongoose");
+
+const appointment = new mongoose.Schema({
+  name: String,
+  email: String,
+  description: String,
+  cpf: String,
+  date: Date,
+  time: String,
+});
+
+module.exports = appointment;
