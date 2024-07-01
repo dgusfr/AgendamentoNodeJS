afd03b1e803ab2b85d2db4b939e91923658ce4d1
dgusfr <diegogustavo.fr@gmail.com>
2024-07-01 10:00:27 -0300

    some changes

--- index.js
+++ index.js
@@ -3,24 +3,21 @@ const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const appointmentService = require("./services/AppointmentService");
+const AppointmentService = require("./services/AppointmentService");
 
-// Configuração
 app.use(express.static("public"));
 
-// Body Parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// EJS
 app.set("view engine", "ejs");
 
-// Conexão com o banco de dados
 mongoose.connect("mongodb://localhost:27017/agendamento", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+mongoose.set("useFindAndModify", false);
 
-// Model - Usuários
 app.get("/", (req, res) => {
   res.render("index");
 });
@@ -30,7 +27,7 @@ app.get("/cadastro", (req, res) => {
 });
 
 app.post("/create", async (req, res) => {
-  const status = await appointmentService.Create(
+  var status = await appointmentService.Create(
     req.body.name,
     req.body.email,
     req.body.description,
@@ -38,14 +35,49 @@ app.post("/create", async (req, res) => {
     req.body.date,
     req.body.time
   );
+
   if (status) {
     res.redirect("/");
   } else {
-    res.send("Ocorreu uma falha");
+    res.send("Ocorreu uma falha!");
   }
 });
 
-// Servidor
-app.listen(8000, () => {
-  console.log("Servidor rodando");
+app.get("/getcalendar", async (req, res) => {
+  var appointments = await AppointmentService.GetAll(false);
+  res.json(appointments);
+});
+
+app.get("/event/:id", async (req, res) => {
+  var appointment = await AppointmentService.GetById(req.params.id);
+  console.log(appointment);
+  res.render("event", { appo: appointment });
+});
+
+app.post("/finish", async (req, res) => {
+  var id = req.body.id;
+  var result = await AppointmentService.Finish(id);
+  res.redirect("/");
+});
+
+app.get("/list", async (req, res) => {
+  //await AppointmentService.Search("343.434.343-43");
+
+  var appos = await AppointmentService.GetAll(true);
+  res.render("list", { appos });
+});
+
+app.get("/searchresult", async (req, res) => {
+  var appos = await AppointmentService.Search(req.query.search);
+  res.render("list", { appos });
+});
+
+var pollTime = 1000 * 60 * 5;
+
+setInterval(async () => {
+  await AppointmentService.SendNotification();
+}, pollTime);
+
+app.listen(3000, () => {
+  console.log("Servidro rodando!");
 });
--- models/Appointment.js
+++ models/Appointment.js
@@ -1,14 +1,14 @@
-const { name } = require("ejs");
 const mongoose = require("mongoose");
 
 const appointment = new mongoose.Schema({
-  name: String,
-  email: String,
-  description: String,
-  cpf: String,
-  date: Date,
-  time: String,
-  finished: Boolean,
+    name: String,
+    email: String,
+    description: String,
+    cpf: String,
+    date: Date,
+    time: String,
+    finished: Boolean,
+    notified: Boolean
 });
 
 module.exports = appointment;
