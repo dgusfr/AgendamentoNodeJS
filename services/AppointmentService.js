532e63bf22a57064be40e2bfe1f6f11bd348cb9c
dgusfr <diegogustavo.fr@gmail.com>
2024-07-01 09:30:23 -0300

    changes

--- services/AppointmentService.js
+++ services/AppointmentService.js
@@ -1,8 +1,22 @@
 var appointment = require("../models/Appointment");
 var mongoose = require("mongoose");
 
+const Appo = mongoose.model("Appointment", appointment);
+
 class AppointmentService {
-  async(name, email, description, cpf, date, time) {}
+  async create(name, email, description, cpf, date, time) {
+    var newAppo = new Appo({
+      name,
+      email,
+      description,
+      cpf,
+      date,
+      time,
+    });
+    try {
+      await newAppo.save();
+    } catch (err) {}
+  }
 }
 
 module.exports = new AppointmentService();
