1b775bf1f8bf1ae612133805283579df0fe853bc
dgusfr <diegogustavo.fr@gmail.com>
2024-07-02 09:43:16 -0300

    changes add factory

--- factories/AppointmentFactory.js
+++ factories/AppointmentFactory.js
@@ -1,27 +1,33 @@
-class AppointmentFactory{
+// A Factory é responsável por processsar e transformar um agendamento simples em um formato específico.
+class AppointmentFactory {
+  // Método para construir um agendamento a partir de um objeto simples de agendamento
+  Build(simpleAppointment) {
+    // Extrai o dia, mês e ano da data do agendamento, ajustando o dia (+1)
+    var day = simpleAppointment.date.getDate() + 1;
+    var month = simpleAppointment.date.getMonth();
+    var year = simpleAppointment.date.getFullYear();
 
-    Build(simpleAppointment){
+    // Extrai a hora e os minutos do horário do agendamento
+    var hour = Number.parseInt(simpleAppointment.time.split(":")[0]);
+    var minutes = Number.parseInt(simpleAppointment.time.split(":")[1]);
 
-        var day = simpleAppointment.date.getDate()+1;
-        var month = simpleAppointment.date.getMonth();
-        var year = simpleAppointment.date.getFullYear();
-        var hour =  Number.parseInt(simpleAppointment.time.split(":")[0]);
-        var minutes = Number.parseInt(simpleAppointment.time.split(":")[1]);
+    // Cria um objeto Date para a data e horário de início do agendamento
+    var startDate = new Date(year, month, day, hour, minutes, 0, 0);
 
-        var startDate = new Date(year,month,day,hour,minutes,0,0);
-        
-        var appo = {
-            id: simpleAppointment._id,
-            title: simpleAppointment.name + " - " + simpleAppointment.description, 
-            start: startDate,
-            end: startDate,
-            notified: simpleAppointment.notified,
-            email: simpleAppointment.email
-        }
-        
-        return appo;
-    }
+    // Cria um novo objeto de agendamento com as propriedades desejadas
+    var appo = {
+      id: simpleAppointment._id,
+      title: simpleAppointment.name + " - " + simpleAppointment.description,
+      start: startDate,
+      end: startDate,
+      notified: simpleAppointment.notified,
+      email: simpleAppointment.email,
+    };
 
+    // Retorna o novo objeto de agendamento
+    return appo;
+  }
 }
 
+// Exporta uma nova instância da AppointmentFactory
 module.exports = new AppointmentFactory();
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Importa os módulos necessários: express para criar o servidor, body-parser para manipulação de dados de requisição,
+// mongoose para manipulação do banco de dados, e o serviço de agendamentos
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
@@ -5,27 +7,34 @@ const mongoose = require("mongoose");
 const appointmentService = require("./services/AppointmentService");
 const AppointmentService = require("./services/AppointmentService");
 
+// Configura o servidor para servir arquivos estáticos da pasta "public"
 app.use(express.static("public"));
 
+// Configura o body-parser para analisar dados URL-encoded e JSON
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Configura o mecanismo de visualização para usar EJS
 app.set("view engine", "ejs");
 
+// Conecta ao banco de dados MongoDB
 mongoose.connect("mongodb://localhost:27017/agendamento", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 mongoose.set("useFindAndModify", false);
 
+// Rota para renderizar a página inicial
 app.get("/", (req, res) => {
   res.render("index");
 });
 
+// Rota para renderizar a página de cadastro
 app.get("/cadastro", (req, res) => {
   res.render("create");
 });
 
+// Rota para criar um novo agendamento
 app.post("/create", async (req, res) => {
   var status = await appointmentService.Create(
     req.body.name,
@@ -43,41 +52,47 @@ app.post("/create", async (req, res) => {
   }
 });
 
+// Rota para obter todos os agendamentos não finalizados em formato JSON
 app.get("/getcalendar", async (req, res) => {
   var appointments = await AppointmentService.GetAll(false);
   res.json(appointments);
 });
 
+// Rota para obter um agendamento específico pelo ID e renderizar a página de evento
 app.get("/event/:id", async (req, res) => {
   var appointment = await AppointmentService.GetById(req.params.id);
   console.log(appointment);
   res.render("event", { appo: appointment });
 });
 
+// Rota para marcar um agendamento como finalizado
 app.post("/finish", async (req, res) => {
   var id = req.body.id;
   var result = await AppointmentService.Finish(id);
   res.redirect("/");
 });
 
+// Rota para listar todos os agendamentos
 app.get("/list", async (req, res) => {
-  //await AppointmentService.Search("343.434.343-43");
-
   var appos = await AppointmentService.GetAll(true);
   res.render("list", { appos });
 });
 
+// Rota para buscar agendamentos por email ou CPF e renderizar a lista de resultados
 app.get("/searchresult", async (req, res) => {
   var appos = await AppointmentService.Search(req.query.search);
   res.render("list", { appos });
 });
 
+// Define o tempo de intervalo para enviar notificações (5 minutos)
 var pollTime = 1000 * 60 * 5;
 
+// Configura um intervalo para enviar notificações periodicamente
 setInterval(async () => {
   await AppointmentService.SendNotification();
 }, pollTime);
 
+// Inicia o servidor na porta 3000
 app.listen(3000, () => {
   console.log("Servidor rodando!");
 });
